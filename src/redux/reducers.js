eaeb33ecbab4cc309ce490569a16d254338167fd
Hussein Alkasake <husseinalkasake@gmail.com>
2019-04-30 03:16:43 -0400

    Close all menus on opening side view

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -19,12 +19,9 @@ const rootReducer = (state = initialState, action) => {
           showSideMenu: action.payload !== undefined && action.payload !== null ? action.payload : !state.showSideMenu
         };
       case CLOSE_MENUS:
-        return {
-          ...state,
-          showMoreInfo: false, showSideMenu: false
-        };
+        return {...state, showMoreInfo: false, showSideMenu: false};
       case UPDATE_MORE_INFO_KEY:
-        return Object.assign({}, state, {showMoreInfo: true, projectMoreInfoKey: action.payload });
+        return Object.assign({}, state, {showMoreInfo: true, showSideMenu: false, projectMoreInfoKey: action.payload });
       default:
         return state;
     }
